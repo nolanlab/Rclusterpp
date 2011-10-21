17e638b04f3ee28778372d0a718bf90bd528b71e
Michael Linderman <michael.d.linderman@gmail.com>
2011-10-21 16:59:05 -0400

    Add example showing Rclusterpp used as a linkable library

--- inst/include/Rclusterpp/hclust.h
+++ inst/include/Rclusterpp/hclust.h
@@ -8,7 +8,20 @@ namespace Rclusterpp {
 	// Initialization and destruction
 
 	template<class Matrix, class Clusters>
-	void init_clusters_from_rows(const Matrix& matrix, Clusters& clusters);
+	Clusters& init_clusters(const Matrix& matrix, Clusters& clusters) {
+		for (ssize_t i=0; i<matrix.rows(); i++) {
+			clusters[i] = Clusters::make_cluster(-(i+1), i);
+		}
+		return clusters;
+	}
+
+	template<class Matrix, class Clusters>
+	Clusters& init_clusters_from_rows(const Matrix& matrix, Clusters& clusters) {
+		for (ssize_t i=0; i<matrix.rows(); i++) {
+			clusters[i] = Clusters::make_cluster(-(i+1), i, matrix.row(i));
+		}
+		return clusters;
+	}
 
 	// Translate clustering results to format expected by R...
 	
@@ -29,8 +42,52 @@ namespace Rclusterpp {
 	};
 		
 	template<class Clusters>
-	void populate_Rhclust(const Clusters& clusters, Hclust& hclust);
+	void populate_Rhclust(const Clusters& clusters, Hclust& hclust) {
+	
+		if (clusters.size() != (2 * hclust.agglomerations() + 1)) {
+			std::invalid_argument("Rclusterpp clusters and hclust object inconsistently sized");
+		}
+
+		typedef typename Clusters::cluster_type cluster_type;
+		typename Clusters::const_iterator last  = clusters.end();
+		typename Clusters::const_iterator first = last - hclust.agglomerations();
+
+		std::transform(first, last, hclust.merge.column(0).begin(), std::mem_fun(&cluster_type::parent1Id));
+		std::transform(first, last, hclust.merge.column(1).begin(), std::mem_fun(&cluster_type::parent2Id));
+		std::transform(first, last, hclust.height.begin(), std::mem_fun(&cluster_type::disimilarity));
+
+		// Compute order that minimizes dendrogram crossings by performing DFS traversal of tree
+		if (first != last) {
+			size_t idx = 0;
+			
+			std::stack<cluster_type const*> stack;
+			stack.push(*(last-1)); // Start with last cluster (that has no children)...
+
+			while (!stack.empty()) {
+				cluster_type const* top = stack.top();
+				stack.pop();
+				if (top->initial()) {
+					(hclust.order)[idx++] = -(top->id());  // Recall we use negative IDs for inital clusters 
+				} else {
+					stack.push(top->parent2());
+					stack.push(top->parent1());
+				}
+			}
+		}
+	}
 
 } // end of Rclusterpp namespace
+
+namespace Rcpp {
+	template <> SEXP wrap( const Rclusterpp::Hclust& hclust ) {
+		return List::create( _["merge"] = hclust.merge, _["height"] = hclust.height, _["order"] = hclust.order ); 
+	}
+	
+	template <typename T> SEXP wrap( const Rclusterpp::ClusterVector<T>& clusters ) {
+		Rclusterpp::Hclust hclust(clusters.initial_clusters());
+		Rclusterpp::populate_Rhclust(clusters, hclust);
+		return Rcpp::wrap(hclust);
+	}
+} // Rcpp namespace
 
 #endif
--- src/hclust.cpp
+++ src/hclust.cpp
@@ -31,72 +31,9 @@ namespace Rcpp {
 			case 4: return Rclusterpp::MINKOWSKI;
 		}
 	}
-
-
-	template <> SEXP wrap( const Rclusterpp::Hclust& hclust ) {
-		return List::create( _["merge"] = hclust.merge, _["height"] = hclust.height, _["order"] = hclust.order ); 
-	}
-
-	template <typename T> SEXP wrap( const Rclusterpp::ClusterVector<T>& clusters ) {
-		Rclusterpp::Hclust hclust(clusters.initial_clusters());
-		Rclusterpp::populate_Rhclust(clusters, hclust);
-		return Rcpp::wrap(hclust);
-	}
 	
 } // Rcpp
 
-namespace Rclusterpp {
-
-	template<class Matrix, class Clusters>
-	void init_clusters(const Matrix& matrix, Clusters& clusters) {
-		for (ssize_t i=0; i<matrix.rows(); i++) {
-			clusters[i] = Clusters::make_cluster(-(i+1), i);
-		}
-	}
-
-	template<class Matrix, class Clusters>
-	void init_clusters_from_rows(const Matrix& matrix, Clusters& clusters) {
-		for (ssize_t i=0; i<matrix.rows(); i++) {
-			clusters[i] = Clusters::make_cluster(-(i+1), i, matrix.row(i));
-		}
-	}
-	
-	template<class Clusters>
-	void populate_Rhclust(const Clusters& clusters, Hclust& hclust) {
-	
-		if (clusters.size() != (2 * hclust.agglomerations() + 1)) {
-			std::invalid_argument("Rclusterpp clusters and hclust object inconsistently sized");
-		}
-
-		typedef typename Clusters::cluster_type cluster_type;
-		typename Clusters::const_iterator last  = clusters.end();
-		typename Clusters::const_iterator first = last - hclust.agglomerations();
-
-		std::transform(first, last, hclust.merge.column(0).begin(), std::mem_fun(&cluster_type::parent1Id));
-		std::transform(first, last, hclust.merge.column(1).begin(), std::mem_fun(&cluster_type::parent2Id));
-		std::transform(first, last, hclust.height.begin(), std::mem_fun(&cluster_type::disimilarity));
-
-		// Compute order that minimizes dendrogram crossings by performing DFS traversal of tree
-		if (first != last) {
-			size_t idx = 0;
-			
-			std::stack<cluster_type const*> stack;
-			stack.push(*(last-1)); // Start with last cluster (that has no children)...
-
-			while (!stack.empty()) {
-				cluster_type const* top = stack.top();
-				stack.pop();
-				if (top->initial()) {
-					(hclust.order)[idx++] = -(top->id());  // Recall we use negative IDs for inital clusters 
-				} else {
-					stack.push(top->parent2());
-					stack.push(top->parent1());
-				}
-			}
-		}
-	}
-
-} // end of Rclusterpp namespace
 
 RcppExport SEXP hclust_from_data(SEXP data, SEXP link, SEXP dist, SEXP minkowski) {
 BEGIN_RCPP
