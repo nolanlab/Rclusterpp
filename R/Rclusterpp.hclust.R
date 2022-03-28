4797bc782e6fdb3b037ac1d64b839aadd293f242
Justin Landis <jtlandis@pop-os.localdomain>
2022-03-28 14:57:54 -0400

    outstanding warning issue

--- R/Rclusterpp.hclust.R
+++ R/Rclusterpp.hclust.R
@@ -20,7 +20,7 @@ Rclusterpp.hclust <- function(x, method="ward", members=NULL, distance="euclidea
   if (method == -1) 
     stop("Ambiguous clustering method")
 
-	if (class(x) == "dist") {
+	if (inherits(x, "dist")) {
 		dist.method = attributes(x)$method
 		labels      = attributes(x)$Labels
 
